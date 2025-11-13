e073beb88bcc37570729ad673910b0d3bf06bfec
Hassanpour fateme <51415176+hspfatemeh@users.noreply.github.com>
2025-11-13 15:24:44 +0330

    Replace time.clock() with time.process_time()

--- attic_util/time_benchmark.py
+++ attic_util/time_benchmark.py
@@ -4,7 +4,7 @@ import logbook
 class Benchmark():
     def __init__(self):
         self.time_wall_start = time.time()
-        self.time_cpu_start = time.clock()
+        self.time_cpu_start = time.process_time()
         self.logger = logbook.Logger(self.__class__.__name__)
 
     def diff_time_wall_secs(self):
@@ -14,5 +14,5 @@ class Benchmark():
         self.logger.info("%s wall=%.3fm cpu=%.3fm" % (
             label,
             self.diff_time_wall_secs() / 60.0,
-            (time.clock() - self.time_cpu_start) / 60.0,
+            (time.process_time() - self.time_cpu_start) / 60.0, 
         ))
